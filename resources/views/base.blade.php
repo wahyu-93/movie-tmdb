9f4014f56c7474655c9c26788f2012db2f1cd641
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-09-01 16:51:14 +0800

    fix home.blade'

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,13 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Bee-Cinema | Watch Movie</title>
+    <title>@yield('title')</title>
 </head>
 <body>
     <div class="w-full h-auto min-h-screen flex flex-col">
         @include('page/header')
-
-        @yield('banner')
 
         @yield('content')
         
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,8 @@
 @extends('base')
 
-@section('banner')
+@section('title', 'Bee-Cinema | Watch Movie')
+
+@section('content')
     @include('page/banner')
     @include('page/top')
 @endsection
