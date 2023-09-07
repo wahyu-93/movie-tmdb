330415a006d7c9190264b3a3f1bb2b7deefc132f
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-09-07 11:13:39 +0800

    page search - fix vote average

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -82,43 +82,43 @@
                 }, 3000);
             }
 
-
-        
             // get data success
             htmlData = [];
             data.results.forEach(item => {
-                if (item.media_type == 'tv'){
-                    id = 'tv/' + item.id
-                    tanggal = new Date(item.first_air_date)
-                    title = item.name
-
+                if(item.media_type == 'movie' || item.media_type == 'tv'){
+                    if (item.media_type == 'tv'){
+                            id = 'tv/' + item.id
+                            tanggal = new Date(item.first_air_date)
+                            title = item.name
+        
+                        }
+                        else {
+                            id = 'movie/' + item.id
+                            tanggal = new Date(item.release_date)
+                            title = item.title
+                        }
+        
+                        htmlData.push(`<a href="${id}" class="group">
+                        <div class="min-w-[232px] min-h-[428px] bg-white drop-shadow-[0_0px_8px_rgba(0,0,0,0.25)] group-hover:drop-shadow-[0_0px_8px_rgba(0,0,0,0.5)] rounded-[32px] p-5 flex flex-col mr-8 duration-100">
+                            <div class="overflow-hidden rounded-[32px]">
+                                <img src="{{ $imageBaseURL }}/w500${item.poster_path}" class="w-full h-[300px] group-hover:scale-125 duration-200">
+                            </div>
+            
+                            <span class="mt-4 line-clamp-1 group-hover:line-clamp-none font-inter font-xl">${title}</span>
+                            <span class="font-inter font-sm">${tanggal.getFullYear()}</span>
+            
+                            <div class="flex flex-row mt-1 items-center">
+                                <span>
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
+                                        <path class="fill-blue-300" fill="#444444" d="M16 7.1c0-1.5-1.4-2.1-2.2-2.1h-2.2c0.4-1 0.7-2.2 0.5-3.1-0.5-1.8-2-1.9-2.5-1.9h-0.1c-0.4 0-0.6 0.2-0.8 0.5l-1 2.8-2.7 2.7h-5v9h5v-1c0.2 0 0.7 0.3 1.2 0.6 1.2 0.6 2.9 1.5 4.5 1.5 2.4 0 3.2-0.3 3.8-1.3 0.3-0.6 0.3-1.1 0.3-1.4 0.2-0.2 0.5-0.5 0.6-1s0.1-0.8 0-1.1c0.2-0.3 0.4-0.7 0.5-1.3 0-0.5-0.1-0.9-0.2-1.2 0.1-0.4 0.3-0.9 0.3-1.7zM2.5 13.5c-0.6 0-1-0.4-1-1s0.4-1 1-1 1 0.4 1 1c0 0.6-0.4 1-1 1zM14.7 9.1c0 0 0.2 0.2 0.2 0.7 0 0.6-0.4 0.9-0.4 0.9l-0.3 0.3 0.2 0.3c0 0 0.2 0.3 0 0.7-0.1 0.4-0.5 0.7-0.5 0.7l-0.3 0.3 0.2 0.4c0 0 0.2 0.4-0.1 0.9-0.2 0.4-0.4 0.7-2.9 0.7-1.4 0-3-0.8-4.1-1.4-0.8-0.4-1.3-0.6-1.7-0.6v0-6h0.1c0.2 0 0.4-0.1 0.6-0.2l2.8-2.8c0.1-0.1 0.1-0.2 0.2-0.3l1-2.7c0.5 0 1.2 0.2 1.4 1.1 0.1 0.6-0.1 1.6-0.6 2.8-0.1 0.3-0.1 0.5 0.1 0.8 0.1 0.2 0.4 0.3 0.7 0.3h2.5c0.1 0 1.2 0.2 1.2 1.1 0 0.8-0.3 1.2-0.3 1.2l-0.3 0.4 0.3 0.4z"></path>
+                                    </svg>
+                                </span>
+            
+                                <span class="font-inter text-sm ml-1">${Math.floor(item.vote_average * 10)}%</span>
+                            </div>
+                        </div>
+                    </a>`)
                 }
-                else {
-                    id = 'movie/' + item.id
-                    tanggal = new Date(item.release_date)
-                    title = item.title
-                }
-
-                htmlData.push(`<a href="${id}" class="group">
-                <div class="min-w-[232px] min-h-[428px] bg-white drop-shadow-[0_0px_8px_rgba(0,0,0,0.25)] group-hover:drop-shadow-[0_0px_8px_rgba(0,0,0,0.5)] rounded-[32px] p-5 flex flex-col mr-8 duration-100">
-                    <div class="overflow-hidden rounded-[32px]">
-                        <img src="{{ $imageBaseURL }}/w500${item.poster_path}" class="w-full h-[300px] group-hover:scale-125 duration-200">
-                    </div>
-    
-                    <span class="mt-4 line-clamp-1 group-hover:line-clamp-none font-inter font-xl">${title}</span>
-                    <span class="font-inter font-sm">${tanggal.getFullYear()}</span>
-    
-                    <div class="flex flex-row mt-1 items-center">
-                        <span>
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
-                                <path class="fill-blue-300" fill="#444444" d="M16 7.1c0-1.5-1.4-2.1-2.2-2.1h-2.2c0.4-1 0.7-2.2 0.5-3.1-0.5-1.8-2-1.9-2.5-1.9h-0.1c-0.4 0-0.6 0.2-0.8 0.5l-1 2.8-2.7 2.7h-5v9h5v-1c0.2 0 0.7 0.3 1.2 0.6 1.2 0.6 2.9 1.5 4.5 1.5 2.4 0 3.2-0.3 3.8-1.3 0.3-0.6 0.3-1.1 0.3-1.4 0.2-0.2 0.5-0.5 0.6-1s0.1-0.8 0-1.1c0.2-0.3 0.4-0.7 0.5-1.3 0-0.5-0.1-0.9-0.2-1.2 0.1-0.4 0.3-0.9 0.3-1.7zM2.5 13.5c-0.6 0-1-0.4-1-1s0.4-1 1-1 1 0.4 1 1c0 0.6-0.4 1-1 1zM14.7 9.1c0 0 0.2 0.2 0.2 0.7 0 0.6-0.4 0.9-0.4 0.9l-0.3 0.3 0.2 0.3c0 0 0.2 0.3 0 0.7-0.1 0.4-0.5 0.7-0.5 0.7l-0.3 0.3 0.2 0.4c0 0 0.2 0.4-0.1 0.9-0.2 0.4-0.4 0.7-2.9 0.7-1.4 0-3-0.8-4.1-1.4-0.8-0.4-1.3-0.6-1.7-0.6v0-6h0.1c0.2 0 0.4-0.1 0.6-0.2l2.8-2.8c0.1-0.1 0.1-0.2 0.2-0.3l1-2.7c0.5 0 1.2 0.2 1.4 1.1 0.1 0.6-0.1 1.6-0.6 2.8-0.1 0.3-0.1 0.5 0.1 0.8 0.1 0.2 0.4 0.3 0.7 0.3h2.5c0.1 0 1.2 0.2 1.2 1.1 0 0.8-0.3 1.2-0.3 1.2l-0.3 0.4 0.3 0.4z"></path>
-                            </svg>
-                        </span>
-    
-                        <span class="font-inter text-sm ml-1">${item.vote_average * 10}%</span>
-                    </div>
-                </div>
-            </a>`)
             })
 
             setTimeout(() => {
